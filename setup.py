b4c44b48f49f6815417bb6317c415e8e5d98a925
Leifur Halldor Asgeirsson <lasgeirsson@zerofail.com>
2016-11-14 11:02:10 -0500

    add setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+
+setup(
+    name="goblin-dse",
+    packages=("goblin_dse",),
+    license="AGPL",
+    url="https://github.com/ZEROFAIL/goblin-dse",
+    author="lasgeirsson",
+    author_email="lasgeirsson@zerofail.com",
+    install_requires=(
+        'goblin',
+    )
+)
